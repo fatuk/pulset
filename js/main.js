28dbf429ffe96e5e0912365fa655f68d20b37261
fatuk <fatukk@gmail.com>
2014-06-29 15:24:51 +0400

    Add icons, fix styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(function() {
+    // Categories dropdown
     var $dropdownBtn = $('.js-dropdownBtn');
     $dropdownBtn.on('click', function(event) {
         event.preventDefault();
         var $this = $(this),
             currentDropdownId = $this.find('a').attr('href');
 
-        $dropdownBtn.removeClass('active');
-        $this.addClass('active');
-        $('.js-dropdown').slideUp('fast');
-        $(currentDropdownId).slideDown('fast');
+        if ($this.hasClass('active')) {
+            $dropdownBtn.removeClass('active');
+            $('.js-dropdown').slideUp('fast');
+        } else {
+            $('.js-dropdown').slideUp('fast');
+            $dropdownBtn.removeClass('active');
+            $this.addClass('active');
+            $(currentDropdownId).slideDown('fast');
+        }
     });
 });
